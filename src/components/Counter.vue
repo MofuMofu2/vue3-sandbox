906e448ddc7ab2f820484e64ddb97cd9f7ebd380
MofuMofu <froakie002@gmail.com>
2022-07-17 15:27:20 +0900

    feature: add lifecycle hook events

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -12,6 +12,22 @@ export default {
   data() {
     return {count: 0}
   },
+  created() {
+    console.info('created', this.$el, this.$data);
+  },
+  beforeMount() {
+    console.info('beforeMount', this.$el, this.$data)
+  },
+  mounted() {
+    console.info('mounted', this.$el, this.$data);
+  },
+  beforeUpdate() {
+    console.info('beforeUpdate', this.$el, this.$data)
+  },
+  updated() {
+    console.info('update', this.$el, this.$data)
+  },
+
   methods: {
     countUp() {
       return this.count = this.count + 1;
